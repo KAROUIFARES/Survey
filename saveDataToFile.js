ba237360d97cc2bf689106fd8f9b8118048dca06
KAROUI-FARES <126524113+KAROUIFARES@users.noreply.github.com>
2023-08-10 11:30:10 +0200

    v1.2

--- saveDataToFile.js
+++ saveDataToFile.js
@@ -7,6 +7,7 @@ app.use(express.json());
 const QuestionnaireData = 'data.json';
 const UserRponseData = 'UserReponse.json';
 const EssaiData='Essai.json';
+const DashbordData='dashbord.json';
 const ReadQuesData = (callback) => {
   fs.readFile(QuestionnaireData, 'utf8', (err, data) => {
     if (err) {
@@ -68,6 +69,78 @@ const readUserReponse = (callback) => {
   });
 };
 
+function readDashbordData(callback) {
+  fs.readFile(DashbordData, 'utf8', (err, data) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        callback(null, []);
+      } else {
+        console.log('Error reading from file:', err);
+        callback(err);
+      }
+    } else {
+      try {
+        const jsonData = JSON.parse(data);
+        callback(null, jsonData);
+      } catch (parseErr) {
+        console.log('Error parsing existing data:', parseErr);
+        callback(parseErr);
+      }
+    }
+  });
+}
+
+function saveDashbordData(data, callback) {
+    readDashbordData((readErr, dashboardData) => {
+      if (readErr) {
+        console.error('Error reading dashboard data:', readErr);
+        callback(readErr);
+        return;
+      }
+  
+      for (let i = 0; i < data.length; i++) {
+        const item = data[i];
+        const foundData = dashboardData.find((dashboardItem) =>
+          dashboardItem.question.QuestionRank === item.QuestionRank
+        );
+  
+        if (foundData) {
+          foundData.question.nbreponse = (foundData.question.nbreponse || 0) + 1;
+  
+          for (const responseLabel of item.reponse) {
+            const foundResponse = foundData.response.find(
+              (responseItem) => responseItem.Label === responseLabel
+            );
+  
+            if (foundResponse) {
+              foundResponse.Value++;
+            }
+          }
+        }
+      }
+  
+      const jsonData = JSON.stringify(dashboardData, null, 2);
+  
+      fs.writeFile(DashbordData, jsonData, 'utf8', (writeErr) => {
+        if (writeErr) {
+          console.error('Error writing to the file:', writeErr);
+          callback(writeErr);
+        } else {
+          console.log('Dashboard data saved successfully!');
+          callback(null);
+        }
+      });
+    });
+  }
+  
+readDashbordData((err, jsonData) => {
+  if (err) {
+    console.error('Error:', err);
+  } else {
+    console.log('Read data:', jsonData);
+  }
+});
+
 /**
  * Questionnaire API 
  * readData ==> Lire tous les questionnaires
@@ -107,7 +180,6 @@ app.get('/readData/:id', (req, res) => {
 });
 app.post('/writeData', (req, res) => {
   const newData = req.body;
-
   ReadQuesData((err, dataArray) => {
     if (err) {
       return res.status(500).json({ error: 'Error reading from the file', err });
@@ -144,14 +216,13 @@ app.post('/StoreUserReponse', (req, res) => {
     }
     dataArray.push(newData);
     const jsonData = JSON.stringify(dataArray, null, 2);
-
     fs.writeFile(UserRponseData, jsonData, 'utf8', (writeErr) => {
       if (writeErr) {
         console.error('Error writing to the file:', writeErr);
         res.status(500).json({ error: 'Error writing to the file', writeErr });
       } else {
-        console.log('Data saved successfully!');
         res.json({ message: 'Data saved successfully' });
+        saveDashbordData(newData);
       }
     });
   });
@@ -182,10 +253,8 @@ app.post('/CreateEssai', (req, res) => {
     });
   });
 });
-
-
 app.put('/InsertUserEmail/:id', (req, res) => {
-  const newEmail = req.body.UserEmail; 
+  const newEmail = req.body.UserEmail;
   const id = req.params.id;
 
   readEssaiData((err, dataArray) => {
@@ -193,12 +262,9 @@ app.put('/InsertUserEmail/:id', (req, res) => {
       return res.status(500).json({ error: 'Error reading from the file', err });
     }
     const userIndex = dataArray.findIndex(item => item._id === id);
-
     if (userIndex !== -1) {
       dataArray[userIndex].UserEmail = newEmail;
-
       const jsonData = JSON.stringify(dataArray, null, 2);
-
       fs.writeFile(EssaiData, jsonData, 'utf8', (writeErr) => {
         if (writeErr) {
           console.error('Error writing to the file:', writeErr);
@@ -213,6 +279,7 @@ app.put('/InsertUserEmail/:id', (req, res) => {
     }
   });
 });
+
 
 
 app.listen(port, () => {
